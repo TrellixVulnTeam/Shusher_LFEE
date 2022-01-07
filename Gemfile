51c0331cfb78aa2e89c5d0b18cbace921ae167ad
judahperillo <judahorli@gmail.com>
2022-01-06 20:24:33 -0500

    sinatra

--- Gemfile
+++ Gemfile
@@ -1,2 +1,3 @@
 source "https://rubygems.org"
+ruby "3.1.0"
 gem "discordrb"
--- shusher.rb
+++ shusher.rb
@@ -1,15 +1,13 @@
-require 'discordrb'
+require "discordrb"
+require "sinatra"
+require "rubygems"
 
-bot = Discordrb::Bot.new token: ENV["SHUSHER_TOKEN"]
+bot = Discordrb::Bot.new token: ENV["SHUSH_TOKEN"]
 
 SHUSH = "\u{1F92B}"
-S = "\u{1F1F8}"
-H = "\u{1F1ED}"
+
 bot.message(with_text: /[\WA-Z]*[A-Z]+[\WA-Z]*/ ) do |event|
 	message = event.message
-	message.react S
-	message.react H
-	message.react H
 	message.react SHUSH
 end
 
