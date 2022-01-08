4c3f7d0c76d8101b4c2d0e7ff929f4f890fb5f54
Judah Perillo <judahorli@gmail.com>
2022-01-07 21:25:22 -0500

    underscore?

--- shusher.rb
+++ shusher.rb
@@ -6,7 +6,7 @@ bot = Discordrb::Bot.new token: ENV["SHUSH_TOKEN"]
 
 SHUSH = "\u{1F92B}"
 
-bot.message(with_text: /[\WA-Z]*[A-Z]+[\WA-Z]*/ ) do |event|
+bot.message(with_text: /[\WA-Z_]*[A-Z]+[\WA-Z_]*/ ) do |event|
 	message = event.message
 	message.react SHUSH
 end
