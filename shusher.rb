e7326953745f8d7b90e49fa461bc82ebbdce448a
Judah Perillo <judahorli@gmail.com>
2022-01-12 12:47:17 -0500

    hm

--- shusher.rb
+++ shusher.rb
@@ -9,7 +9,7 @@ POPPER = "\u{1F389}"
 
 bot.message(with_text: /[\WA-Z_]*[A-Z]+[\WA-Z_]*/ ) do |event|
 	message = event.message
-	if event.user == "abnoormality#7816"
+	if event.user == "abnoormality#7816" || event.user == "boycryptid#8398"
 		message.react SHUSH		
 	else
 		message.react POPPER
