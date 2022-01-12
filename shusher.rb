c80de7e56b209be8047d283eb7f4f82ab39ff135
Judah Perillo <judahorli@gmail.com>
2022-01-12 09:19:01 -0500

    shusher is now cheerer except for noor, maybe

--- shusher.rb
+++ shusher.rb
@@ -4,11 +4,16 @@ require "rubygems"
 
 bot = Discordrb::Bot.new token: ENV["SHUSH_TOKEN"]
 
-SHUSH = "\u{1F389}"
+SHUSH = "\u{1F92B}"
+POPPER = "\u{1F389}"
 
 bot.message(with_text: /[\WA-Z_]*[A-Z]+[\WA-Z_]*/ ) do |event|
 	message = event.message
-	message.react SHUSH
+	if event.user.username == "abnoormality#7816"
+		message.react SHUSH		
+	else
+		message.react POPPER
+	end
 end
 
 bot.run
