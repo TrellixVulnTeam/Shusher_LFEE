b858bad8076ecaa0ca5da99084be6b7da9938eed
Judah Perillo <judahorli@gmail.com>
2022-03-16 15:05:41 -0400

    only sometimes pop

--- shusher.rb
+++ shusher.rb
@@ -9,7 +9,7 @@ POPPER = "\u{1F389}"
 
 bot.message(with_text: /[\WA-Z_]*[A-Z]+[\WA-Z_]*/ ) do |event|
 	message = event.message
-	num = rand(2)
+	num = rand(3)
 	if event.user.username == ENV["SHUSH_USER"]
 		message.react SHUSH		
 	elsif num == 2
